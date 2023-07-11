1c84144afae252368693d2edc3ca5a71d2947f03
R4mBLeR <99557962+R4mBLeR@users.noreply.github.com>
2023-07-12 00:22:16 +0300

    v1.0

--- bot/main.py
+++ bot/main.py
@@ -2,6 +2,7 @@ import config
 import discord
 from discord.ext import commands
 from commands_init import init
+from discord.ext.commands import CommandNotFound
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -13,6 +14,12 @@ async def on_ready():
     init(bot)
     print('Бот запущен')
 
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, CommandNotFound):
+        return
+    raise error
 
 if (__name__ == "__main__"):
     bot.run(config.TOKEN)
